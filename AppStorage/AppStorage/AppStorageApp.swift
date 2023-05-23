1b328953bced893f3c772e689ad2fa793a2a0298
Ravija Gunawardena <59413517+RavijaGunawardena@users.noreply.github.com>
2023-05-23 16:48:06 +0530

    Add files via upload

---
+++ AppStorage/AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Ravija Gunawardana on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
